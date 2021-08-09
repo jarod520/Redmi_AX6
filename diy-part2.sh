2085bfaf25b755a0c034daae1f4fae208ec52b0c
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-08-09 13:55:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,13 +22,14 @@ sed -i 's/%D %V, %C/%D %V, '$date' By Jarod Chang/g' package/base-files/files/et
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 #设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# 修改插件名字
+sed -i 's/"带宽监控"/"带宽"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 # themes添加（svn co 命令意思：指定版本如https://github）
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #移除不用软件包  
 rm -rf package/lean/luci-app-ttyd
 rm -rf feeds/packages/utils/ttyd
 #添加额外非必须软件包
-#git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 svn co https://github.com/jarod360/packages/trunk/ttyd package/ttyd
